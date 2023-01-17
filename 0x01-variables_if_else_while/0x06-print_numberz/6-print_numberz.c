02eca430008680f8916624649925abfa97512840
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-17 17:06:05 +0000

    print number

---
+++ 0x01-variables_if_else_while/0x06-print_numberz/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+* Return: Always 0
+*
+*/
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+
+	return (0);
+}
