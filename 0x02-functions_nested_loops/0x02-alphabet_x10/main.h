2bda767e3dd833fcf109b77789c004d9aadc09ee
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-21 14:00:43 +0000

    code fix

--- 0x02-functions_nested_loops/0x02-alphabet_x10/main.h
+++ 0x02-functions_nested_loops/0x02-alphabet_x10/main.h
@@ -1,8 +1,7 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-#include <stlib.h>
+#include <unistd.h>
 
 int _putchar(char);
 void print_alphabet_x10(void);
