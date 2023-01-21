f8bce97aaaccfd6d99c0c835a635b9bf02f6aa3b
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-21 11:01:47 +0000

    prints lower alphabets

---
+++ 0x02-functions_nested_loops/0x01-alphabet/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int lower_alpha = 'a';
+
+	while lower_alpha <= 'z'
+	{
+		_putchar(lower_alpha);
+		lower_alpha++;
+	}
+}
