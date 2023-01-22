5f5b5f9278e422cc56e3c5f1d6b8d3edbc42a433
Ayodele Kadiri <kadelcode@gmail.com>
2023-01-22 06:20:45 +0000

    header file

---
+++ 0x02-functions_nested_loops/0x07-print_last_digit/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* standard libraries */
+#include <unistd.lib>
+
+/* function prototypes */
+int print_last_digit(int);
+int _putchar(char);
+
+#endif /* MAIN_H */
